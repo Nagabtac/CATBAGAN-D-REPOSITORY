4413c8db1bb29a7427ea46ed95dc69668d5478ea
Nagabtac <darwin.catbagan@lorma.edu>
2025-01-15 13:49:10 +0800

    fixed i++

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld{
     public static void main (String []args ){
         
-        for(int i = 1; i <=10;){
-            System.out.print("HelloWorld");
+        for(int i = 1; i <=10; i++){
+            System.out.print("\nHelloWorld");
 
         }
     }
