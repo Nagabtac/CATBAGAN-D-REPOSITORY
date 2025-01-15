b64296b7429e1a42e1cfc93f28a4bbd9fe6e85a7
Nagabtac <darwin.catbagan@lorma.edu>
2025-01-15 13:22:45 +0800

    fixed forloop

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
     public static void main (String []args ){
         
-        for(int i = 1; i <= 10){
+        for(int i = 1; i <=10;){
             System.out.print("HelloWorld");
 
         }
